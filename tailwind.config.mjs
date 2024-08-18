77b86b857eaa1a5b2c2021fae9583e99143f73db
Pablo Valencia <59809785+coronapl@users.noreply.github.com>
2024-08-18 15:48:53 -0600

    feat: create blog header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Libre Franklin'", "sans-serif"],
+        serif: ["'Source Serif Pro'", "serif"],
+      },
+    },
+    colors: {
+      paper: "#e9e4d9",
+      ink: "#383732",
+    },
+  },
+  plugins: [],
+};
